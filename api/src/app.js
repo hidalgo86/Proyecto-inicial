0c0449bdc83d416f962a76df47616f4e22b9d19e
hidalgo86 <95661987+hidalgo86@users.noreply.github.com>
2022-08-22 18:04:09 -0300

    7

--- api/src/app.js
+++ api/src/app.js
@@ -5,6 +5,7 @@ const morgan = require("morgan");
 const routes = require("./routes/index.js");
 const cors = require("cors");
 require("./db.js");
+const { CORS_URL } = process.env
 
 const server = express();
 
@@ -16,7 +17,7 @@ server.use(bodyParser.json({ limit: "50mb" }));
 server.use(cookieParser());
 server.use(morgan("dev"));
 server.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", CORS_URL); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Credentials", "true");
   res.header(
     "Access-Control-Allow-Headers",
