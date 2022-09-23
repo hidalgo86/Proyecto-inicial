d9a6c72f2b770bdb9d78f4754ce51d63f301a7aa
hidalgo86 <95661987+hidalgo86@users.noreply.github.com>
2022-09-23 18:11:43 -0300

    prueba

--- client/src/components/Start/Start.jsx
+++ client/src/components/Start/Start.jsx
@@ -14,7 +14,7 @@ export default function Start() {
 
   return (
     <div className={styles.conteiner}>
-      <b className={styles.titulo}>App Dog</b>
+      <b className={styles.titulo}>App Dogs</b>
       <div className={styles.img}>
         <img src={logo} alt="" />
       </div>
